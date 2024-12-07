bfd3d9a2f9a10e843c3b2dbac1eb6e8332d38156
ck <33865803+tonyckc@users.noreply.github.com>
2024-12-07 21:43:22 +0800

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from .algorithms import *
-
-
-def get_algorithm_class(algorithm_name):
-    """Return the algorithm class with the given name."""
-    if algorithm_name not in globals():
-        raise NotImplementedError("Algorithm not found: {}".format(algorithm_name))
-    return globals()[algorithm_name]
